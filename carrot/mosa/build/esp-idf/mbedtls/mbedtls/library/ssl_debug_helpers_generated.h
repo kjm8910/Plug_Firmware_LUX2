c43c70e217950c9cede40358b77636d612b928a9
Finn <jm89.kim@gmail.com>
2023-01-16 15:15:12 +0900

    First Commit

---
+++ carrot/mosa/build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.h
@@ -0,0 +1,1 @@
+/Users/jm.kim/Desktop/carrot/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.h
---
+++ carrot/mosa/build/esp-idf/mbedtls/mbedtls/library/version_features.c
@@ -0,0 +1,1 @@
+/Users/jm.kim/Desktop/carrot/esp-idf/components/mbedtls/mbedtls/library/version_features.c
